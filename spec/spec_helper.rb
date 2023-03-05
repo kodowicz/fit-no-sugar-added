336da75639c112240c386168f99e033af2d769da
Anna Kotowicz <anna.julia.kotowicz@gmail.com>
2023-03-05 14:56:22 +0100

    chore: add debug gem (#2)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ SPEC_ROOT = Pathname(__dir__).realpath.freeze
 
 ENV["HANAMI_ENV"] ||= "test"
 require "hanami/prepare"
+require "debug"
 
 require_relative "support/rspec"
 require_relative "support/requests"
